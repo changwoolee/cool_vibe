55f26958fe11c54dd72ab0b77e3619f45e6e9d87
changwoolee <owga30@gmail.com>
2017-09-03 06:20:00 +0000

    initial commit

---
+++ pyglove.py
@@ -0,0 +1,26 @@
+from bluepy.btle import Peripheral, DefaultDelegate
+
+rwHandle = 0x0025
+
+class NotifyHandler(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleNotification(self,cHandle,data):
+        if cHandle==rwHandle:
+            print(data)
+
+leftHand = Peripheral("a4:d5:78:0d:07:a2")
+#rightHand = btle.Peripheral(" MAC Addr Here ")"
+
+#characteristics = btle.getCharacteristics(Handle)
+
+leftHand.withDelegate(NotifyHandler())
+
+while True:
+    leftHand.writeCharacteristic(rwHandle,"1234")
+    if leftHand.waitForNotifications(1.0):
+        continue
+
+    print("Waiting...")
+
